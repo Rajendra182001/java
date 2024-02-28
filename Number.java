99918c063f6b2a2cd3e6cca70dd736de346b2f5a
Rajendra182001 <rajendra.xworkz@gmail.com>
2024-02-28 13:41:02 +0530

    committing number file

---
+++ Number.java
@@ -0,0 +1,20 @@
+class Number{
+
+            public static void main(String numb[]){
+			
+			int sum = ();
+ }
+			
+			public static void Array(){
+			int sum[]= {2,7,6,5};
+			int result = 9;
+			for (int index=0; index>=number.length;index++)
+			}	
+	        if (int num[0]+ int num[1] == 9){ 
+            System.out.println("result"); 
+			}		
+          if else{  
+        System.out.println("result");  
+    }  
+}  
+}  
